3b543a8e0bf882087d792a4f165788701a285016
summer <yuu.hyelim@gmail.com>
2024-01-25 10:01:08 +0900

    240124

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// function slide(){
+//   $('.sliderimg').stop().animate({marginLeft:-1100},800,function(){
+//     $('.sliderimg div:first').appendTo('.sliderimg');
+//     $('.sliderimg').css({marginLeft:0});
+//   });
+// }
+
+// setInterval(slide,5000);
