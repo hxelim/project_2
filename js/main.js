3fdc0a833c1bffde223d031ae267380c51ea8907
summer <yuu.hyelim@gmail.com>
2024-02-06 14:11:34 +0900

    240206

--- js/main.js
+++ js/main.js
@@ -1,47 +1,53 @@
-$(function(){
-  function updateSlideTextHeight(){
-  var 자식높이 = $('.boxSize').height();
-  $('.slidetext').css('height', 자식높이)
-}
-    updateSlideTextHeight();
+$(function () {
+  function updateSlideTextHeight() {
+    var 자식높이 = $('.boxSize').height();
+    $('.slidetext').css('height', 자식높이)
+  }
+  updateSlideTextHeight();
 
-  $(window).resize(function(){
+  $(window).resize(function () {
     updateSlideTextHeight();
   });
+})
 
-// 호버하면 navigation
-  $('nav li, .Mnav').mouseover(function(){
+
+$(function () {
+  // 호버하면 navigation
+  $('nav li, .Mnav').mouseover(function () {
     $('.emptybox').stop().slideDown();
     $('.sub_nav').stop().slideDown();
-  }).mouseout(function(){
+  })
+  .mouseout(function () {
     $('.emptybox').stop().slideUp();
     $('.sub_nav').stop().slideUp();
   })
 
-  $('.sub_nav li a').mouseover(function(){
+  $('.sub_nav li a').mouseover(function () {
     $(this).addClass('select')
-  }).mouseout(function(){
+  }).mouseout(function () {
     $(this).removeClass('select')
   })
+})
+
+$(function(){
+  // 슬라이드 사진 
+  function slide() {
+    $('.sliderimg').stop().animate({ marginLeft: -1110 }, 900, function () {
+      $('.sliderimg div:first').appendTo('.sliderimg');
+      $('.sliderimg').css({ marginLeft: 0 });
+    });
+    $('.stext').stop().animate({ marginLeft: -1110 }, 900, function () {
+      $('.stext h6:first').appendTo('.stext');
+      $('.stext').css({ marginLeft: 0 });
+    });
+  }
+
+  setInterval(slide, 5000);
+});
 
 
-
-// 슬라이드 사진 
-function slide() {
-  $('.sliderimg').stop().animate({ marginLeft: -1110 }, 900, function () {
-    $('.sliderimg div:first').appendTo('.sliderimg');
-    $('.sliderimg').css({ marginLeft: 0 });
-  });
-  $('.stext').stop().animate({ marginLeft: -1110 }, 900, function () {
-    $('.stext h6:first').appendTo('.stext');
-    $('.stext').css({ marginLeft: 0 });
-  });
-}
-
-setInterval(slide, 5000);
-
-
-// 스크롤 내리면 article 아래에서 등장
+$(function(){
+  // 스크롤 내리면 article 아래에서 등장
   $(window).scroll(function () {
     var ws = $(this).scrollTop();
     var p1 = $(".part1").offset().top;
@@ -61,11 +67,11 @@ setInterval(slide, 5000);
 
 
 
-// 스크롤 내려가면 part2 나타나는 효과
+  // 스크롤 내려가면 part2 나타나는 효과
   $(window).scroll(function () {
     var ws = $(this).scrollTop();
     var p2 = $(".part2").offset().top;
-    var windowWidth=$(window).width();
+    var windowWidth = $(window).width();
 
     if (p2 < ws) {
       $('.bg2023').css("transform", "translateY(-70px)");
@@ -74,34 +80,38 @@ setInterval(slide, 5000);
     }
 
 
-    if (windowWidth >=1457){
- 
+    if (windowWidth >= 1457) {
+
       if (p2 < ws) {
 
-      $("#videobox .video").width("90%");
-      $("#videobox p").width("90%");
-      $("#videobox .container").width("1440px");
-    }
+        $("#videobox .video").width("90%");
+        $("#videobox p").width("90%");
+        $("#videobox .container").width("1440px");
+      }
 
-    if (ws > p2 + 850) {
+      if (ws > p2 + 850) {
+        $("#videobox .video").width("100%");
+        $("#videobox p").width("100%");
+        $("#videobox .container").width("100%");
+
+
+      }
+
+      if (ws > p2 + 1150) {
+        $("#videobox .video").width("90%");
+        $("#videobox p").width("90%");
+        $("#videobox .container").width("1440px");
+      }
+    } else {
       $("#videobox .video").width("100%");
       $("#videobox p").width("100%");
       $("#videobox .container").width("100%");
-
-
     }
 
-    if (ws > p2 + 1150) {
-      $("#videobox .video").width("90%");
-      $("#videobox p").width("90%");
-      $("#videobox .container").width("1440px");
-    }
-  } else {
-    $("#videobox .video").width("100%");
-    $("#videobox p").width("100%");
-    $("#videobox .container").width("100%");
-  }
-// 포토슬라이드
+
+    
+    if (windowWidth >= 1457) {
+    // 포토슬라이드
     if (ws > p2 + 1400) {
 
       $('#photoslide .ps1').css("transform", "translateY(0px)").css("opacity", "1");
@@ -117,7 +127,7 @@ setInterval(slide, 5000);
     }
 
     if (ws > p2 + 1750) {
-      $('#photoslide .textbox').css("transform", "translateY(0px)").css("opacity", "0.8");
+      $('#photoslide .textbox').css("transform", "translateY(0px)").css("opacity", "1");
 
     }
 
@@ -131,33 +141,41 @@ setInterval(slide, 5000);
     if (ws > p2 + 1950) {
       $('#photoslide .ps6').css("transform", "translateY(0px)").css("opacity", "1");
     }
+  } else {
+    if (ws > p2 + 1400) {
 
+      $('#photoslide .imgboxBG').children().css("transform", "translateY(0px)").css("opacity", "1");}
+
+      if (ws > p2 + 1650) {
+      $('#photoslide .textbox').css("transform", "translateY(0px)").css("opacity", "1");
+    }
+  }
   });
 
 
-// 스크롤 내려가면 part3 나타나는 효과
+  // 스크롤 내려가면 part3 나타나는 효과
   $(window).scroll(function () {
     var ws = $(this).scrollTop();
     var p3 = $(".part3").offset().top;
 
-    if (ws> p3-700) {
-      $('#french .box').eq(0).animate({top:-100, opacity:1},500);
-      $('#french .box').eq(1).delay(300).animate({top:-100, opacity:1},500);
-      $('#french .box').eq(2).delay(500).animate({top:-100, opacity:1},500);
-      $('#french .box').eq(3).delay(800).animate({top:-100, opacity:1},500);
+    if (ws > p3 - 700) {
+      $('#french .box').eq(0).animate({ top: -100, opacity: 1 }, 500);
+      $('#french .box').eq(1).delay(300).animate({ top: -100, opacity: 1 }, 500);
+      $('#french .box').eq(2).delay(500).animate({ top: -100, opacity: 1 }, 500);
+      $('#french .box').eq(3).delay(800).animate({ top: -100, opacity: 1 }, 500);
     }
 
-    if (ws > p3 ) {
-      $('#black').width((ws-7000)/11+'%')
+    if (ws > p3) {
+      $('#black').width((ws - 7000) / 11 + '%')
 
     }
 
 
-    
+
   });
 
 
-// 수상내역 마우스 호버시 포스터 바뀜
+  // 수상내역 마우스 호버시 포스터 바뀜
   $('.awardslist').mouseenter(function () {
     let idx = $(this).index()
 
@@ -170,7 +188,7 @@ setInterval(slide, 5000);
   });
 
 
-// 포토슬라이드 버튼 호버하면 이미지 어둡게
+  // 포토슬라이드 버튼 호버하면 이미지 어둡게
   $('#photoslide button').mouseenter(function () {
     $('.imgboxBG').fadeTo('50ms', 0.3);
   })
